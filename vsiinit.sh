06b5c9333381e309e447a84a12b9615528439b98
kosugi811 <75505698+kosugi811@users.noreply.github.com>
2020-12-05 09:33:02 +0900

    Update vsiinit.sh

--- vsiinit.sh
+++ vsiinit.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-/usr/sbin/ip route add 172.16.0.0/12 via 10.133.98.69 dev eth0
+# /usr/sbin/ip route add 172.16.0.0/12 via 10.133.98.69 dev eth0
+/usr/bin/sed -i s/GATEWAY=161.202.249.1/GATEWAY=161.202.249.2/g /etc/sysconfig/network-scripts/ifcfg-eth1
